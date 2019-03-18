927320d654b3df29fcc525941f6c083f1eacc9fd
Mua Tuuau <tuuauromymua@gmail.com>
2019-03-19 05:30:45 +1300

    version 1 - Mua - Shippable Test

---
+++ signup.php
@@ -0,0 +1,146 @@
+<?php 
+
+session_start();
+
+if(isset($_SESSION['id_user']) || isset($_SESSION['id_company'])) { 
+  header("Location: index.php");
+  exit();
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Everdi - Signup</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="css/AdminLTE.min.css">
+  <link rel="stylesheet" href="css/_all-skins.min.css">
+  <!-- Custom -->
+  <link rel="stylesheet" href="css/custom.css">
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition skin-purple sidebar-mini">
+<div class="wrapper">
+
+  <header class="main-header">
+
+    <!-- Logo -->
+    <a href="index.php" class="logo logo-bg">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini">Everdi</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><img src="img/logo.jpg" height="52" width="148" /></span>
+    </a>
+
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Navbar Right Menu -->
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li>
+            <a href="review.php">Reviews</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper" style="margin-left: 0px;">
+
+    <section class="content-header">
+      <div class="container">
+        <div class="row margin-top-50 margin-bottom-20">
+          <h1 class="text-center margin-bottom-20">Sign Up</h1>
+          <form method="post" id="signup-user" action="adduser.php" enctype="multipart/form-data">
+            <div class="col-md-6 col-md-offset-3">
+              <div class="form-group">
+                <input class="form-control input-lg" type="text" id="firstname" name="firstname" placeholder="First Name *" required ="">
+              </div>
+              <div class="form-group">
+                <input class="form-control input-lg" type="text" id="lastname" name="lastname" placeholder="Last Name *" required>
+              </div>
+              <div class="form-group">
+                <input class="form-control input-lg" type="text" id="email" name="email" placeholder="Email *" required>
+              </div>
+              <div class="form-group">
+                <input class="form-control input-lg" type="password" id="password" name="password" placeholder="Password *" required>
+              </div>
+              <div class="form-group">
+                <input class="form-control input-lg" type="password" id="cpassword" name="cpassword" placeholder="Confirm Password *" required>
+              </div>
+              <div id="passwordError" class="btn btn-flat btn-danger hide-me" >
+                    Please match both passwords!
+              </div>                   
+              <div class="form-group">
+                <button class="btn btn-flat btn-success">Register</button>
+              </div>
+              <?php 
+              //If User already registered with this email then show error message.
+              if(isset($_SESSION['registerError'])) {
+                ?>
+                <div class="form-group">
+                  <label style="color: red;">Email Already Exists! Please Choose A Different Email!</label>
+                </div>
+              <?php
+               unset($_SESSION['registerError']); }
+              ?> 
+
+            </div>            
+
+          </form>
+          
+        </div>
+      </div>
+    </section>
+
+    
+
+  </div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer" style="margin-left: 0px;">
+    <div class="text-center">
+      <strong>Copyright &copy; 2018 <a href="#"> Everdi</a>.</strong> Developed by Romy Mua Tuuau. All rights reserved. 
+    </div>
+  </footer>
+
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="js/adminlte.min.js"></script>
+
+
+<script>
+  $("#signup-user").on("submit", function(e) {
+    e.preventDefault();
+    if( $('#password').val() != $('#cpassword').val() ) {
+      $('#passwordError').show();
+    } else {
+      $(this).unbind('submit').submit();
+    }
+  });
+</script>
+</body>
+</html>
